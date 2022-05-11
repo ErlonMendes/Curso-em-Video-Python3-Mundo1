0e685739558a4d39f033eb33a936d9312bf7691d
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-11 10:57:35 -0300

    Add files via upload

---
+++ aula6desafio3.py
@@ -0,0 +1,4 @@
+n1 = float(input('Escreva um número '))
+n2 = float(input('Escreva outro número '))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
---
+++ aula6inputtype.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um número ')
+print(type(n1))
+n1 = float(n1)
+print(type(n1))
