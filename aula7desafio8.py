a851d848a4c8f9b73e5dc33765a71a13786e44e5
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-13 17:25:21 -0300

    Delete aula7desafio8.py

--- aula7desafio8.py
+++
@@ -1,6 +0,0 @@
-dist = float(input('Digite a distância (m) '))
-centi = dist*1e2
-mili = dist*1e3
-foot = dist*3.28084
-inch = dist*39.3701
-print('{} m = {} cm = {} mm = {} ft = {} in'.format(dist, centi, mili, foot, inch))
