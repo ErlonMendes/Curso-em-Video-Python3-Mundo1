062cd1715c0687561cf2910177bce1a89370fc7d
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-13 17:32:22 -0300

    Add files via upload

---
+++ aula7desafio8.py
@@ -0,0 +1,11 @@
+dist = float(input('Digite a distância (m) '))
+km = dist/1e3
+hm = dist/1e2
+dam = dist/10
+dm = dist*10
+cm = dist*1e2
+mm = dist*1e3
+ft = dist*3.28084
+inch = ft*12
+print('{} m = {} km = {} hm = {} dam = {} dm = {} cm = {} mm = {} ft = {} in'
+      .format(dist, km, hm, dam, dm, cm, mm, ft, inch))
