d6ca02b5ed6698bbea26412454f3f410d58713ec
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-15 10:58:59 -0300

    Add files via upload

---
+++ exercicio15.py
@@ -0,0 +1,6 @@
+dias = int(input('Por quantos dias o carro foi alugado? '))
+kmi = float(input('Qual foi a quilometragem marcada na saída do carro? '))
+kmf = float(input('Qual foi a quilometragem marcada na devolução do carro? '))
+km = kmf - kmi
+aluguel = dias*60 + km*0.15
+print('O carro rodou {:.1f} km durante {} dias e o aluguel foi de R$ {:.2f}'.format(km, dias, aluguel))
