f943922f93cb941a8d6e0a833b7c3e783a45a811
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-14 22:30:18 -0300

    Delete aula7desafio11.py

--- aula7desafio11.py
+++
@@ -1,5 +0,0 @@
-larg = float(input('Digite a largura da parede '))
-alt = float(input('Digite a altura da parede '))
-area = larg*alt
-n = area/2
-print('Sua parede tem {} m^2 e precisará de {} L de tinta para pintá-la'.format(area, n))
