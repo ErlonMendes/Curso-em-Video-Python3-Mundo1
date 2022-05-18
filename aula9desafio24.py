21f221db3d875ff85375398ac1dfeb9de5e9c46a
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-18 20:52:34 -0300

    Add files via upload

---
+++ aula9desafio24.py
@@ -0,0 +1,3 @@
+cidade = input('Digite o nome de uma cidade: ').strip()
+print('A cidade começa com a palavra SANTO? {}'.format('SANTO' in cidade[:5].upper()))
+# print('A cidade começa com a palavra SANTO? {}'.format(cidade[:5].upper() == 'SANTO'))
