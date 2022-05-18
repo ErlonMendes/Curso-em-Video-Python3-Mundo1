91a1f7024e44ee7c4cb1433ce4f517ee6d3cf12b
Erlon Mendes <103445012+ErlonMendes@users.noreply.github.com>
2022-05-17 23:36:04 -0300

    Delete aula8desafio21.py

--- aula8desafio21.py
+++
@@ -1,6 +0,0 @@
-import pygame  # Precisou instalar o módulo externo pygame
-pygame.init()
-pygame.mixer.music.load('aula8desafio21.mp3')
-pygame.mixer.music.play()
-input()  # Alteração necessária devido à atualização do pygame
-pygame.event.wait()
